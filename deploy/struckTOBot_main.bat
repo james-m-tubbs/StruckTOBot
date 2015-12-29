76e1ec68c0c0a58360878d56ee566ecfa3ef6123
James Mak-Tubbs <james.m.tubbs@gmail.com>
2015-12-29 00:49:22 -0500

    Still won't read conf from classpath

--- deploy/struckTOBot_main.bat
+++ deploy/struckTOBot_main.bat
@@ -2,7 +2,7 @@ SET echo ON
 SET homedir=D:\Dev\git\StruckTOBot\deploy
 SET main=ca.gkwb.struckto.main.StruckTOMain
 
-SET classpath=%homedir%\lib\*;%homedir%\conf\context.xml;%homedir%\conf\twitter4j.properties
+SET classpath=%homedir%\*;%homedir%\lib\*;%homedir%\conf\context.xml;%homedir%\conf\twitter4j.properties
 echo Classpath = %classpath%
 
-java %main% TPSOperations 100 #BikeTO,#WalkTO -Dlog4j.configuration=%homedir%\logs\log4j.properties -cp=%classpath%
+java %main% TPSOperations 100 #BikeTO,#WalkTO -Dlog4j.configuration=%homedir%\conf\log4j.properties -cp=%classpath%
