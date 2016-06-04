2c8e89500c169687305a129eaa7aa60917a63ed5
James Mak-Tubbs <james.m.tubbs@gmail.com>
2016-06-04 00:37:42 -0400

    Added twitterVO structure to java

---
+++ src/ca/gkwb/struckto/vo/StruckTOTweetVO.java
@@ -0,0 +1,27 @@
+package ca.gkwb.struckto.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+public class StruckTOTweetVO {
+	
+//	"TWEET_ID" integer NOT NULL PRIMARY KEY,
+	@Getter @Setter
+	private int tweetId;
+//	"TWEET_URL" character varying,
+	@Getter @Setter
+	private String url;
+//	"TWEET_ACCOUNT" character varying,
+	@Getter @Setter
+	private String account;
+//	"TWEET_TEXT" character varying,
+	@Getter @Setter
+	private String text;
+//	"TWEET_TIMESTAMP" date)
+	private Date timestamp;
+
+}
