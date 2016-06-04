b3e76277bb6390c02581109de68ec35bd5cf92ab
James Mak-Tubbs <james.m.tubbs@gmail.com>
2016-06-03 22:43:51 -0400

    Added/Updated DAO interfaces

---
+++ src/ca/gkwb/struckto/dao/StruckTOLocationDAO.java
@@ -0,0 +1,38 @@
+package ca.gkwb.struckto.dao;
+
+import ca.gkwb.struckto.exception.GenericDBException;
+import ca.gkwb.struckto.vo.StruckTOLocationVO;
+
+public interface StruckTOLocationDAO {
+	
+	/**
+	 * Inserts a single StruckTOLocationVO based on input object
+	 * 
+	 * @author g1ng3rk1d
+	 * @date 2016-06-03
+	 * @param StruckTOLocationVO
+	 * @throws GenericDBException
+	 */
+	public void insertStruckTOLocation(StruckTOLocationVO stlVO) throws GenericDBException;;
+
+	/**
+	 * Updates a single StruckTOLocationVO based on input object
+	 * 
+	 * @author g1ng3rk1d
+	 * @date 2016-06-03
+	 * @param StruckTOLocationVO
+	 * @throws GenericDBException
+	 */
+	public void updateStruckTOLocation(StruckTOLocationVO stlVO) throws GenericDBException;;
+	
+	/**
+	 * Queries single StruckTOLocationVO based on input ID
+	 * 
+	 * @author g1ng3rk1d
+	 * @date 2016-06-03
+	 * @param int
+	 * @return StruckTOLocationVO
+	 * @throws GenericDBException
+	 */
+	public StruckTOLocationVO queryOneLocation(int id) throws GenericDBException; 
+}
