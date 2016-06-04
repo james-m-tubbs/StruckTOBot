00d4e260bae56fa6e394aac2a372d153c4c6586e
James Mak-Tubbs <james.m.tubbs@gmail.com>
2016-06-03 22:19:45 -0400

    Added initial schema SQL

--- DBScripts/schema-init.sql
+++ DBScripts/schema-init.sql
@@ -9,6 +9,48 @@ create user StruckTODBUser password '';
 grant all on schema StruckTODB to StruckTODBUser;
 grant all on all tables in schema StruckTODB to StruckTODBUser;
 
---Create Schema
+--Clean old schema
+DROP TABLE strucktodb."INCIDENT";
+DROP TABLE strucktodb."LOCATION";
 
+--Create Schema
+--Create Location
+CREATE TABLE strucktodb."LOCATION"
+(
+   "LOCATION_ID" SERIAL NOT NULL PRIMARY KEY, 
+   "LOCATION_LAT" real NOT NULL, 
+   "LOCATION_LONG" real NOT NULL, 
+   "LOCATION_CITY" character varying, 
+   "LOCATION_PROV" character varying, 
+   "LOCATION_COUNTRY" character varying, 
+   "LOCATION_CREATE_DATE" date NOT NULL, 
+   "LOCATION_USER" character varying
+) 
+WITH (
+  OIDS = FALSE
+)
+
+TABLESPACE pg_default;
+
+--Create Incident
+
+CREATE TABLE strucktodb."INCIDENT"
+(
+  "INCIDENT_ID" serial NOT NULL,
+  "INCIDENT_TWEET" character varying,
+  "INCIDENT_TWEET_TEXT" character varying,
+  "INCIDENT_SEVERITY" character varying,
+  "INCIDENT_NEWS_URL" character varying,
+  "INCIDENT_CREATE_DATE" date NOT NULL,
+  "INCIDENT_ACTIVITY_DATE" date NOT NULL,
+  "INDICDENT_LOCATION_ID" integer,
+  CONSTRAINT "INCIDENT_pkey" PRIMARY KEY ("INCIDENT_ID"),
+  CONSTRAINT "FK_LOCATION" FOREIGN KEY ("INDICDENT_LOCATION_ID")
+      REFERENCES strucktodb."LOCATION" ("LOCATION_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+)
+TABLESPACE pg_default;
 
