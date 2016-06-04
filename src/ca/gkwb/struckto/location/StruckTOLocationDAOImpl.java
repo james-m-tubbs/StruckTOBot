013e84e9536846573ec3cb59810c61d4f3860e61
James Mak-Tubbs <james.m.tubbs@gmail.com>
2016-06-04 01:38:40 -0400

    Added stub for StruckTOLocationDAO implementation

---
+++ src/ca/gkwb/struckto/location/StruckTOLocationDAOImpl.java
@@ -0,0 +1,22 @@
+package ca.gkwb.struckto.location;
+
+import ca.gkwb.struckto.exception.GenericDBException;
+
+public class StruckTOLocationDAOImpl implements StruckTOLocationDAO {
+
+	public void insertStruckTOLocation(StruckTOLocationVO stlVO) throws GenericDBException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateStruckTOLocation(StruckTOLocationVO stlVO) throws GenericDBException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public StruckTOLocationVO queryOneLocation(int id) throws GenericDBException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
