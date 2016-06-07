6e56f9ce2e88f213891a340490181f2f1b3a9420
James Mak-Tubbs <james.m.tubbs@gmail.com>
2016-06-07 00:24:29 -0400

    Started work on DAO layer

--- src/ca/gkwb/struckto/location/StruckTOLocationDAOImpl.java
+++ src/ca/gkwb/struckto/location/StruckTOLocationDAOImpl.java
@@ -1,12 +1,34 @@
 package ca.gkwb.struckto.location;
 
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import ca.gkwb.struckto.exception.GenericDBException;
 
 public class StruckTOLocationDAOImpl implements StruckTOLocationDAO {
-
+	
+	private JdbcTemplate conn;
+	private final String INSERT_SQL = "";
+	private final String UPDATE_SQL = "";
+	private final String QUERY_SQL = "SELECT " +
+			   "LOCATION_ID," +
+			   "LOCATION_LAT," +  
+			   "LOCATION_LONG," +  
+			   "LOCATION_CITY," + 
+			   "LOCATION_PROV," + 
+			   "LOCATION_COUNTRY," + 
+			   "LOCATION_CREATE_DATE," + 
+			   "LOCATION_USER," +			
+			 "FROM LOCATION " +
+			 "WHERE LOCATION_ID = ? ";
+	
+	public StruckTOLocationDAOImpl(JdbcTemplate conn) {
+		this.conn = conn;
+	}
+	
 	public void insertStruckTOLocation(StruckTOLocationVO stlVO) throws GenericDBException {
-		// TODO Auto-generated method stub
-
 	}
 
 	public void updateStruckTOLocation(StruckTOLocationVO stlVO) throws GenericDBException {
@@ -15,8 +37,28 @@ public class StruckTOLocationDAOImpl implements StruckTOLocationDAO {
 	}
 
 	public StruckTOLocationVO queryOneLocation(int id) throws GenericDBException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		
+		try {
+	        List<StruckTOLocationVO> result = conn.query(QUERY_SQL, new Object[] { id },
+	        		(rs, rowNum) -> new StruckTOLocationVO(
+	        			rs.getInt("LOCATION_ID"),	
+	        			rs.getFloat("LOCATION_LAT"),
+	        			rs.getFloat("LOCATION_LONG"),
+	        			rs.getString("LOCATION_CITY"),
+	        			rs.getString("LOCATION_PROV"),
+	        			rs.getString("LOCATION_COUNTRY"),
+	        			rs.getDate("LOCATION_CREATE_DATE"),
+	        			rs.getString("LOCATION_USER")
+	        		));
+	        
+	        //should only get one result
+	        if (result.size() > 1) throw new GenericDBException("More than one result for " + id);
+	        if (result.size() < 1) return null;
+	        return result.get(0);
+	        
+       } catch (DataAccessException e) { 
+    	   throw new GenericDBException(e);
+       }
+	};
 
 }
--- src/ca/gkwb/struckto/location/StruckTOLocationVO.java
+++ src/ca/gkwb/struckto/location/StruckTOLocationVO.java
@@ -8,21 +8,36 @@ import lombok.Setter;
 
 @AllArgsConstructor
 public class StruckTOLocationVO {
+
+	//"LOCATION_ID," +
+	@Getter @Setter
+	private int id;
 	
+   //"LOCATION_LAT," +
 	@Getter @Setter
-	private String location;
+	private float lat;
+	
+//  "LOCATION_LONG," +
 	@Getter @Setter
-	private float xCoords;
-	@Getter @Setter
-	private float yCoords;
+	private float lng;
+	
+//  "LOCATION_CITY," +
 	@Getter @Setter
 	private String city;
+	
+//  "LOCATION_PROV," +
 	@Getter @Setter
-	private String province;
+	private String prov;
+	
+//  "LOCATION_COUNTRY," +
 	@Getter @Setter
 	private String country;
+	
+//  "LOCATION_CREATE_DATE," +
 	@Getter @Setter
-	private Date createDate;
+	private Date create_date;
+	
+//  "LOCATION_USER,"
 	@Getter @Setter
-	private String user;
+	private String user;	
 }
