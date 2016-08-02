3fbb801cb897166a5e4ede2eff38dedd6ec19ead
James Mak-Tubbs <james.m.tubbs@gmail.com>
2016-08-01 23:25:44 -0400

    Added Test HSQL Scripts

---
+++ DBScripts/test-schema-init.sql
@@ -0,0 +1,44 @@
+--Create Schema
+--Create Location
+create schema strucktodb authorization dba;
+
+CREATE TABLE strucktodb."LOCATION"
+(
+   "LOCATION_ID" INTEGER NOT NULL PRIMARY KEY, 
+   "LOCATION_LAT" real NOT NULL, 
+   "LOCATION_LONG" real NOT NULL, 
+   "LOCATION_CITY" character varying, 
+   "LOCATION_PROV" character varying, 
+   "LOCATION_COUNTRY" character varying, 
+   "LOCATION_CREATE_DATE" date NOT NULL, 
+   "LOCATION_USER" character varying
+);
+
+--added to ensure we keep locations unique
+alter table strucktodb."LOCATION" ADD CONSTRAINT pk_incident UNIQUE ("LOCATION_LAT","LOCATION_LONG");
+
+--create tweet capture
+CREATE TABLE strucktodb."TWEET"
+(
+  "TWEET_ID" bigint NOT NULL PRIMARY KEY,
+  "TWEET_URL" character varying,
+  "TWEET_ACCOUNT" character varying,
+  "TWEET_TIMESTAMP" date);
+
+--Create Incident
+CREATE TABLE strucktodb."INCIDENT"
+(
+  "INCIDENT_ID" INTEGER NOT NULL,
+  "INCIDENT_TWEET_ID" bigint,
+  "INCIDENT_SEVERITY" character varying,
+  "INCIDENT_NEWS_URL" character varying,
+  "INCIDENT_CREATE_DATE" date NOT NULL,
+  "INCIDENT_ACTIVITY_DATE" date NOT NULL,
+  "INDICDENT_LOCATION_ID" integer,
+  "INCIDENT_VERIFIED" character varying,
+  CONSTRAINT "INCIDENT_pkey" PRIMARY KEY ("INCIDENT_ID"),
+  CONSTRAINT "FK_LOCATION" FOREIGN KEY ("INDICDENT_LOCATION_ID")
+      REFERENCES strucktodb."LOCATION" ("LOCATION_ID"),
+  CONSTRAINT "FK_TWEET" FOREIGN KEY ("INCIDENT_TWEET_ID")
+      REFERENCES strucktodb."TWEET" ("TWEET_ID") 
+);
