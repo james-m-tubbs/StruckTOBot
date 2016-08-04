909f99502ae5ea3b7f7b2c6bc8dc5fb831c28bca
James Mak-Tubbs <james.m.tubbs@gmail.com>
2016-08-04 00:52:19 -0400

    Added missing tests

---
+++ test/ca/gksb/struckto/config/test/StruckTOTestConfig.java
@@ -0,0 +1,21 @@
+package ca.gksb.struckto.config.test;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@Profile("Test")
+public class StruckTOTestConfig {
+	@Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.HSQL)
+            .addScript("classpath:schema-init.sql")
+            .build();
+    }
+}
---
+++ test/ca/gkwb/struckto/incident/StruckTOIncidentDAOTest.java
@@ -0,0 +1,48 @@
+package ca.gkwb.struckto.incident;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
+
+public class StruckTOIncidentDAOTest {
+
+	StruckTOIncidentDAO stiDAO;
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+	
+	@Before
+	public void setUp() throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		System.setProperty("spring.profiles.active", "test1");
+		stiDAO = (StruckTOIncidentDAO)context.getBean("struckTOIncidentDAO");
+	}	
+	
+	@Test
+	public void testQueryById() {
+		try {
+			StruckTOIncidentVO stiVO = stiDAO.queryById(1);
+			Assert.assertTrue(stiVO.getIncidentId() == 1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	@Transactional
+	public void testInsert() {
+		try {
+			//TODO
+			stiDAO.insert(null);
+			
+			StruckTOIncidentVO stiVO = stiDAO.queryById(1);
+			Assert.assertTrue(stiVO.getIncidentId() == 1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}	
+}
